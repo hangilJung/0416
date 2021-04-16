33ee56f162e832495c94163e582728b49b7244a0
hangiljung <gksrlf0331@gamail.com>
2021-04-16 16:51:32 +0900

    프런트 단 javascript 파일 추가

--- app2/app.js
+++ app2/app.js
@@ -10,8 +10,8 @@ const home = require('./src/routes/home');
 // 앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
-
+app.use(express.static(`${__dirname}/src/public`));  //js폴더 접근을 위해 설정을 해준다.
 
 app.use('/', home);
 
-module.exports = app;
+module.exports = app; 
