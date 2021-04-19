bbe85af40a16a037e750150e2457c1b70bf4027e
hangiljung <gksrlf0331@gamail.com>
2021-04-19 12:12:50 +0900

    회원가입 페이지 구현

--- app2/src/routes/home/index.js
+++ app2/src/routes/home/index.js
@@ -8,6 +8,7 @@ const ctrl = require("./home.ctrl");
 
 router.get("/",  ctrl.output.home);//경로가 기본적으로 ./views이므로 생략가능
 router.get("/login", ctrl.output.login);
+router.get("/register", ctrl.output.register);
 router.post("/login", ctrl.process.login );
 
 module.exports = router;
